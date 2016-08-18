4bddd7fc351cc5464ce58539296a17f83501a404
Ben Maddison <benm@workonline.co.za>
2016-08-18 23:01:52 +0200

    added packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+version = open('packaging/VERSION').read().strip()
+requirements = open('packaging/requirements.txt').read().split("\n")
+# test_requirements = open('packaging/requirements-test.txt').read().split("\n")
+
+setup(
+    name='prngmgr',
+    version=version,
+    author='Workonline Communications',
+    author_email='communications@workonkonline.co.za',
+    description='Django webapp for peering session management using PeeringDB API',
+    long_description='',
+    license='LICENSE',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+    ],
+    packages=find_packages(
+        include=[
+            'prngmgr',
+            'prngmgr.*'
+            'test'
+        ],
+        exclude=[
+            'prngmgr.migrations'
+        ]
+    ),
+    include_package_data=True,
+
+    url='https://github.com/wolcomm/prngmgr',
+    download_url='https://github.com/prngmgr/%s' % version,
+
+    install_requires=requirements,
+    # test_requires=test_requirements,
+    test_suite='test.exec'
+)
