354c0cfa0d61c085fc6be70b2662a5befaddb17c
Ben Maddison <benm@workonline.co.za>
2016-09-15 02:49:45 +0200

    added login decorator to index

--- prngmgr/views/index.py
+++ prngmgr/views/index.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 from django_peeringdb.models.concrete import Network
 from prngmgr import settings
 
 me = Network.objects.get(asn=settings.MY_ASN)
 
 
+@login_required
 def index(request):
     template = loader.get_template('prngmgr/index.html')
     context = {
