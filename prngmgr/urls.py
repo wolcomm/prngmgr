61e2cd4d586f51bb4ba5afe739f969d6f59cfa0f
Ben Maddison <benm@workonline.co.za>
2016-10-03 10:24:39 +0200

    removed depricated views

--- prngmgr/urls.py
+++ prngmgr/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url, include
 from prngmgr import classy_views
-from prngmgr.views import index, sessions, routers, interfaces, ixps, networks
+from prngmgr.views import routers, interfaces
 
 
 urlpatterns = [
     url(r'^$', classy_views.IndexView.as_view(), name="prngmgr-index"),
     url(r'^sessions/(?P<pk>\d+)?$', classy_views.PeeringSessionsView.as_view(), name="prngmgr-sessions"),
     url(r'^networks/(?P<pk>\d+)?$', classy_views.NetworksView.as_view(), name="prngmgr-networks"),
-    url(r'^routers/(?P<rtr_id>\d+)?$', routers.routers, name="prngmgr-routers"),
     url(r'^ixps/(?P<pk>\d+)?$', classy_views.InternetExchangeView.as_view(), name="prngmgr-ixps"),
+    url(r'^routers/(?P<rtr_id>\d+)?$', routers.routers, name="prngmgr-routers"),
     url(r'^interfaces/(?P<if_id>\d+)?(?P<if_delete>/delete)?$', interfaces.interfaces, name="prngmgr-interfaces"),
     url(r'^api/', include('prngmgr.api.urls'))
 ]
