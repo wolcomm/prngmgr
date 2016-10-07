a20ba511064faf8da2a100e56d6ec20c0fa0ed68
Ben Maddison <benm@workonline.co.za>
2016-10-07 20:05:41 +0200

    added af aware snmp retrieval

--- prngmgr/snmp.py
+++ prngmgr/snmp.py
@@ -41,3 +41,70 @@ def get_bgp_table(host):
         elif results[address]['cbgpPeer2Type'] == 2:
             results[address]['cbgpPeer2RemoteAddr'] = ipaddress.IPv6Address(int(address, 16))
     return results
+
+
+def get_bgp_state(host):
+    peer_table = ObjectIdentity('CISCO-BGP4-MIB', 'cbgpPeer2Table')
+    af_table = ObjectIdentity('CISCO-BGP4-MIB', 'cbgpPeer2AddrFamilyTable')
+    prefix_table = ObjectIdentity('CISCO-BGP4-MIB', 'cbgpPeer2AddrFamilyPrefixTable')
+    target = UdpTransportTarget((host, SNMP['port']))
+
+    results = defaultdict(dict)
+
+    for (errorIndication, errorStatus,
+         errorIndex, varBinds) in bulkCmd(snmp, usm, target, context, 0, 25, ObjectType(peer_table),
+                                          lexicographicMode=False, lookupMib=True):
+        if errorIndication:
+            raise RuntimeError(errorIndication)
+        elif errorStatus:
+            raise RuntimeError('%s at %s' % (errorStatus.prettyPrint(),
+                                             errorIndex and varBinds[-1][int(errorIndex)-1] or '?'))
+        else:
+            for (key, val) in varBinds:
+                if not isinstance(val, EndOfMibView):
+                    (mib, name, index) = key.loadMibs('BGP4-MIB').getMibSymbol()
+                    address = index[1].prettyPrint()
+                    results[address]['cbgpPeer2Type'] = index[0]
+                    results[address][name] = val
+    for address in results:
+        results[address]['address_families'] = dict()
+    for (errorIndication, errorStatus,
+         errorIndex, varBinds) in bulkCmd(snmp, usm, target, context, 0, 25, ObjectType(af_table),
+                                          lexicographicMode=False, lookupMib=True):
+        if errorIndication:
+            raise RuntimeError(errorIndication)
+        elif errorStatus:
+            raise RuntimeError('%s at %s' % (errorStatus.prettyPrint(),
+                                             errorIndex and varBinds[-1][int(errorIndex) - 1] or '?'))
+        else:
+            for (key, val) in varBinds:
+                if not isinstance(val, EndOfMibView):
+                    (mib, name, index) = key.loadMibs('BGP4-MIB').getMibSymbol()
+                    address = index[1].prettyPrint()
+                    afi = index[2].prettyPrint()
+                    safi = index[3].prettyPrint()
+                    afi_safi_name = "%s.%s" % (afi, safi)
+                    results[address]['address_families'][afi_safi_name] = {name: val}
+    for (errorIndication, errorStatus,
+         errorIndex, varBinds) in bulkCmd(snmp, usm, target, context, 0, 25, ObjectType(prefix_table),
+                                          lexicographicMode=False, lookupMib=True):
+        if errorIndication:
+            raise RuntimeError(errorIndication)
+        elif errorStatus:
+            raise RuntimeError('%s at %s' % (errorStatus.prettyPrint(),
+                                             errorIndex and varBinds[-1][int(errorIndex) - 1] or '?'))
+        else:
+            for (key, val) in varBinds:
+                if not isinstance(val, EndOfMibView):
+                    (mib, name, index) = key.loadMibs('BGP4-MIB').getMibSymbol()
+                    address = index[1].prettyPrint()
+                    afi = index[2].prettyPrint()
+                    safi = index[3].prettyPrint()
+                    afi_safi_name = "%s.%s" % (afi, safi)
+                    results[address]['address_families'][afi_safi_name][name] = val
+    for address in results:
+        if results[address]['cbgpPeer2Type'] == 1:
+            results[address]['cbgpPeer2RemoteAddr'] = ipaddress.IPv4Address(int(address, 16))
+        elif results[address]['cbgpPeer2Type'] == 2:
+            results[address]['cbgpPeer2RemoteAddr'] = ipaddress.IPv6Address(int(address, 16))
+    return results
