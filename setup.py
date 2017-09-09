8e320253622ef0192a816a73d13055308d0925eb
Ben Maddison <benm@workonline.co.za>
2017-09-09 13:30:38 +0200

    fix up packaging and testing stuff

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ def readme():
 
 version = open('packaging/VERSION').read().strip()
 requirements = open('packaging/requirements.txt').read().split("\n")
-# test_requirements = open('packaging/requirements-test.txt').read().split("\n")
 
 setup(
     name='prngmgr',
@@ -24,20 +23,11 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet',
     ],
-    packages=find_packages(
-        include=[
-            'prngmgr',
-            'prngmgr.*',
-            'test'
-        ],
-        exclude=[]
-    ),
+    packages=find_packages(),
     include_package_data=True,
 
     url='https://github.com/wolcomm/prngmgr',
     download_url='https://github.com/prngmgr/%s' % version,
 
     install_requires=requirements,
-    # test_requires=test_requirements,
-    test_suite='test.exec'
 )
---
+++ tests/conftest.py
@@ -0,0 +1,75 @@
+# Copyright 2017 Workonline Communications (Pty) Ltd. All rights reserved.
+#
+# The contents of this file are licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""Test configuration setup for prngmgr."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+def pytest_configure():
+    """Configure pytest."""
+    from django.conf import settings
+
+    MIDDLEWARE = (
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    )
+
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        SITE_ID=1,
+        SECRET_KEY='not very secret in tests',
+        USE_I18N=True,
+        USE_L10N=True,
+        STATIC_URL='/static/',
+        ROOT_URLCONF='tests.urls',
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+            },
+        ],
+        MIDDLEWARE=MIDDLEWARE,
+        MIDDLEWARE_CLASSES=MIDDLEWARE,
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.staticfiles',
+            'django_peeringdb',
+            'rest_framework',
+            'prngmgr',
+        ),
+        PASSWORD_HASHERS=(
+            'django.contrib.auth.hashers.MD5PasswordHasher',
+        ),
+        COUNTRIES_OVERRIDE={
+           'XK': 'Kosovo',
+        },
+    )
+
+    try:
+        import django
+        django.setup()
+    except AttributeError:
+        pass
