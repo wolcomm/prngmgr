d51283d0d9b454ea13e1200fb9ac66b5d1ed94ad
Ben Maddison <benm@workonline.co.za>
2017-09-09 12:54:26 +0200

    centralise settings in project

--- prngmgr/settings.py
+++ prngmgr/settings.py
@@ -1,1 +1,10 @@
-from prngmgr.local_settings import *
+from django.conf import settings
+
+MY_ASN = getattr(settings, 'PRNGMGR_MY_ASN', None)
+
+SNMP = getattr(settings, 'PRNGMGR_SNMP', {
+   'port': 161,
+   'user': None,
+   'authPass': None,
+   'privKey': None,
+})
